ae2cc678f96a90ab47a4fb4a2d20b3ea16105443
Azrod60 <becerramathieu.60@gmail.com>
2024-09-10 11:17:31 +0200

    first commit

---
+++ back/db.js
@@ -0,0 +1,24 @@
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config({ path: '.secrets' });
+}
+
+const pg = require("pg");
+const { Pool } = pg;
+const pool = new Pool({
+  user: "postgres",
+  password: process.env.POSTGRES_PASSWORD,
+  host: "localhost", // = 127.0.0.1
+  port: 5441,
+  database: "postgres",
+});
+
+async function query(req, params) {
+  try {
+    return await pool.query(req, params);
+  } catch (err) {
+    return err;
+  }
+}
+
+module.exports = query;
+module.exports = pool;
